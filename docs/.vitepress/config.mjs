4de4892c62a885c3da5aed3f12882833ddb4fbf5
4triumph <864195969@qq.com>
2024-09-29 12:43:34 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { getSidebar } from "./utils/getSidebar";
 
 export default defineConfig({
+  base: "/docs-blog/",
   // 标签上显示的网站标题
   title: "4triumph",
   titleTemplate: "4triumph's docs",
